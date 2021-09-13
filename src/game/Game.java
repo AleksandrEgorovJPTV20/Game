affd14736ea668451112a7a3b2f00fe8dc8f41b9
unknown <arokai1233@mail.ru>
2021-09-13 16:38:19 +0300

    Улучшил программу, исправил ошибки и добавил выбор продолжить игру или нет

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,48 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package game;
 
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author pupil
- */
 public class Game {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        int attempt = 4;
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int number = random.nextInt(10);
-        System.out.print("Программа задумала число от 0 до 9! Попробуй отгадать: ");
-        //for(int i = 0; i < 5; i++) {
+        
         int i = 0;
-        do {
-            int userNumber = scanner.nextInt();
-            if(userNumber == number) {
-                System.out.println("Ура! Ты выиграл!");
-                break;
-            }else{
-                if(i < attempt) {
-                    System.out.println("Неправильно, попробуй еще! ");
-            }   
-                
-                else {
-                    System.out.println(":(, проиграл, задумано: "+number);
+        boolean victory = false;
+        String choice = "yes";
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        while(choice.equals("yes")){
+            int randomNum = random.nextInt(10);
+
+            System.out.println("Программа загадала число от 0 до 9 пять раз. Отгадайте cлучайное число: ");
+            
+            do{
+                int userNumber = scanner.nextInt();
+
+                if(userNumber == randomNum){
+                    System.out.println("Вы победили!");
+                    victory = true;
+                    break;
+                }else{
+                    System.out.println("Неверно, попробуйте ещё раз!\n");
+                }
+                if(i==4){
                     break;
                 }
-              
+                i++;
+            }while(true);
+            
+            if (!victory) {
+                System.out.println("Попытки закончились! Случайное число: " + randomNum + "\n");
             }
-            i++;
-        } while(true);
+            System.out.println("Введите yes, чтобы продолжить. \nВведите no, чтобы выйти из программы.");
+            choice = scanner.next();
+        }
     }
 }
-        
